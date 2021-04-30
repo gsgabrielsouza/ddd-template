aaec7b2b89d05a5e2a7cece022ed318f3af47ee1
gabriel souza <souza.gabrielluis@gmail.com>
2021-04-29 22:45:16 -0300

    add examples in domain

---
+++ src/ddd.template.Domain/Extensions/QueryableExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ddd.template.Domain.Extensions
+{
+    public static class QueryableExtension
+    {
+        public static IQueryable<TEntity> Filter<TEntity>(
+             this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> expressionCondition)
+        {
+            return query.Where(expressionCondition);
+        }
+
+        public static IQueryable<TEntity> Filter<TEntity>(
+            this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> expressionCondition, Expression<Func<bool>> conditionAttribute)
+        {
+            var func = conditionAttribute.Compile();
+            bool canAssingValue = (bool)func();
+            if (canAssingValue)
+            {
+                return query.Filter(expressionCondition);
+            }
+
+            return query;
+        }
+    }
+}
---
+++ src/ddd.template.Infra/Context/Map/AccountMap.cs
@@ -0,0 +1,18 @@
+﻿using ddd.template.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace SiteMercado.Payment.Service.Infra.Map
+{
+    /// <summary>
+    /// Example
+    /// </summary>
+    public class AccountMap : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
